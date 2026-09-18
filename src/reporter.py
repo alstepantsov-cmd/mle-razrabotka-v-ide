a5ec9261e59aeb3e1c5fae4f7f46a96c89950f56
Alexey Stepantsov <al.stepantsov@gmail.com>
2026-09-18 14:10:59 +0300

    Add_show_report

--- src/reporter.py
+++ src/reporter.py
@@ -3,3 +3,23 @@ class DataFrameReporter:
         self.float_format = float_format
         self.percent_format = percent_format
         self.include_all = include_all
+
+    def show_report(self, df, title=None):
+            if title:
+                print(title)
+        
+            print('Количество столбцов:', df.shape[1])
+            print('Количество строк:', df.shape[0])
+
+            duplicates = df.duplicated().sum()
+            print('Количество дубликатов:', duplicates)
+
+            print('Доля дубликатов:', format(duplicates / df.shape[0], self.percent_format))
+
+import pandas as pd
+
+data = pd.read_csv("D:/YandexDisk/ML_Inginier/Lessons/Sprint_6/Project_split_6/mle-razrabotka-v-ide/data/payments.csv")
+
+reporter = DataFrameReporter(float_format='0.02f', percent_format='0.03%')
+
+reporter.show_report(data, 'Отчёт в формате 1:')
